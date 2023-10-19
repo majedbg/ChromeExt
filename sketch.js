d40ec50df2a6ef1679e6f73905d6d5033e860559
Jade BuGhanem <majed.bg@gmail.com>
2023-10-19 09:27:46 -0400

    committing all files

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+// function setup() {
+// let c = createCanvas(windowWidth,windowHeight);
+// c.position(0,0);
+// c.style('pointer-events','none');
+// //clear();
+// }
+
+// function draw() {
+// clear();
+// resizeCanvas(windowWidth, windowHeight);
+//   background(123, 211, 255, 100);
+//   strokeWeight(4);
+// line(mouseX,mouseY,mouseX,0);
+// circle(mouseX,mouseY,30);
+// }
+
+
